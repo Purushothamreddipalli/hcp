659615c9e09da68465f40778afa743e81b7985f0
Purushothamreddipalli <r.p.r.531219@gmail.com>
2025-02-12 21:00:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
 resource "aws_instance" "one" {
   ami           = "ami-03b8adbf322415fd0"
   instance_type = "t2.micro"
